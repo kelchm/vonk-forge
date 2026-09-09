ad67117a8b630abdafe988766aa439cc6203e7ed
Matthew Kelch <matt@kelch.io>
2026-09-09 01:16:30 -0400

    Remove retired non-null cache projections from schema-2 databases

--- control/tests/test_catalog_documents_postgres.py
+++ control/tests/test_catalog_documents_postgres.py
@@ -78,7 +78,7 @@ def test_fresh_postgres_migration_builds_canonical_schema(postgres_engine) -> No
         "recipe_revisions",
     }.isdisjoint(inspector.get_table_names())
     with postgres_engine.connect() as connection:
-        assert connection.exec_driver_sql("SELECT version_num FROM alembic_version").scalar_one() == "0024_failure_evidence"
+        assert connection.exec_driver_sql("SELECT version_num FROM alembic_version").scalar_one() == "0025_retired_cache_fields"
 
 
 def test_valid_model_write_read_and_projection_is_postgres_backed(catalog) -> None:
